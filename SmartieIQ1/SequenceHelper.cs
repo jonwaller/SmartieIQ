49b95d1e161bfe7c5fc7de21b80901c457c7484f
jonwaller <jon@jonwaller.net>
2011-09-20 22:27:20 +0900

    Adding tags, and making a few more files versioned - Whoops. :)

---
+++ SmartieIQ1/SequenceHelper.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmartieIQ
+{
+	static class SequenceHelper
+	{
+
+		public class CouldNotFindGeneratorFunctionException : System.Exception
+		{
+			public CouldNotFindGeneratorFunctionException() : base() { }
+		}  
+
+		public static bool isSame(List<int?> list1, List<int?> list2)
+		{
+			if (list1.Count != list2.Count) return false;
+
+			for (int i = 0; i < list1.Count; i++)
+			{
+				if (list1[i] != list2[i])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public static bool isSame(List<int> list1, List<int> list2)
+		{
+			if (list1.Count != list2.Count) return false;
+
+			for (int i = 0; i < list1.Count; i++)
+			{
+				if (list1[i] != list2[i])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		/* Real code :) */
+
+
+
+		private static Func<int, int> identityFunction = new Func<int, int>((i) => i);
+
+		public static List<Func<int, int>> basic1ArgFuncs =
+
+			new List<Func<int, int>>{
+				new Func<int, int>((i) =>i), //important
+				new Func<int, int>((i) =>-1),
+				new Func<int, int>((i) =>1),
+				new Func<int, int>((i) =>i%2)
+		};
+
+		static double goldenRatio = (1 + Math.Sqrt(5.0)) / 2.0;
+
+		public static List<Func<int, int, int>> basic2ArgFuncs =
+	new List<Func<int, int, int>>{
+		new Func<int, int, int>(
+            (i, j) =>i+j
+        ),
+         new Func<int, int, int>(
+             (i, j) =>i*j
+        ),
+		new Func<int, int, int>(
+             (i, j) =>(int)(Math.Pow(i,j))
+        )/*,
+        		new Func<int, int, int>(
+             (i, j) =>(int)(Math.Pow(i*(1-goldenRatio),j))
+        ),
+				new Func<int, int, int>(
+             (i, j) =>(int)(Math.Pow(i*goldenRatio,j))
+        )*/
+    };
+		public  static List<int> fillMissing(List<int?> sequenceWithNulls)
+		{
+			Func<int, int> itemGeneratorFunction = findItemGeneratorFunction(sequenceWithNulls);
+
+			if (itemGeneratorFunction == null) throw new CouldNotFindGeneratorFunctionException();
+
+			return create(itemGeneratorFunction, sequenceWithNulls.Count);
+		}
+
+		public static List<int> create(Func<int, int> function, int lengthRequested)
+		{
+			return map(function, getIdentitySequence(lengthRequested));
+		}
+
+		public static List<int> getIdentitySequence(int lengthRequested)
+		{
+			List<int> identitySequence = new List<int>();
+			for (int i = 0; i < lengthRequested; i++)
+			{
+				identitySequence.Add(i);
+			}
+			return identitySequence;
+		}
+
+
+		public static bool createMakesMatchingSequence(Func<int, int> function, List<int?> questionSequence)
+		{
+			for (int i = 0; i < questionSequence.Count; i++)
+			{
+				if (questionSequence[i] != null)
+				{
+					if (function(i) != questionSequence[i])
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+
+
+	
+
+
+		public  static Func<int, int> findItemGeneratorFunction(List<int?> questionSequence)
+		{
+			//foreach (Func<int, int> sf15 in basic1ArgFuncs)
+			//{
+			foreach (Func<int, int, int> sop7 in basic2ArgFuncs)
+			{
+				//foreach (Func<int, int> sf14 in basic1ArgFuncs)
+				//{
+				foreach (Func<int, int, int> sop6 in basic2ArgFuncs)
+				{
+					//foreach (Func<int, int> sf13 in basic1ArgFuncs)
+					//{
+					foreach (Func<int, int, int> sop5 in basic2ArgFuncs)
+					{
+						foreach (Func<int, int> sf12 in basic1ArgFuncs)
+						{
+							foreach (Func<int, int> sf11 in basic1ArgFuncs)
+							{
+								//foreach (Func<int, int> sf10 in basic1ArgFuncs)
+								//{
+								foreach (Func<int, int, int> sop4 in basic2ArgFuncs)
+								{
+									foreach (Func<int, int> sf9 in basic1ArgFuncs)
+									{
+										foreach (Func<int, int> sf8 in basic1ArgFuncs)
+										{
+											//foreach (Func<int, int> sf7 in basic1ArgFuncs)
+											//{
+											foreach (Func<int, int, int> sop3 in basic2ArgFuncs)
+											{
+												//foreach (Func<int, int> sf6 in basic1ArgFuncs)
+												//{
+												foreach (Func<int, int, int> sop2 in basic2ArgFuncs)
+												{
+													foreach (Func<int, int> sf5 in basic1ArgFuncs)
+													{
+														foreach (Func<int, int> sf4 in basic1ArgFuncs)
+														{
+															//foreach (Func<int, int> sf3 in basic1ArgFuncs)
+															//{
+															foreach (Func<int, int, int> sop1 in basic2ArgFuncs)
+															{
+																foreach (Func<int, int> sf2 in basic1ArgFuncs)
+																{
+																	foreach (Func<int, int> sf1 in basic1ArgFuncs)
+																	{
+																		//This functions need to be copied, or the previous items in the list will be "updated" to the new (wrong) function
+																		Func<int, int, int> op1 = new Func<int, int, int>(sop1);
+																		Func<int, int, int> op2 = new Func<int, int, int>(sop2);
+																		Func<int, int, int> op3 = new Func<int, int, int>(sop3);
+																		Func<int, int, int> op4 = new Func<int, int, int>(sop4);
+																		Func<int, int, int> op5 = new Func<int, int, int>(sop5);
+																		Func<int, int, int> op6 = new Func<int, int, int>(sop6);
+																		Func<int, int, int> op7 = new Func<int, int, int>(sop7);
+
+																		Func<int, int> f1 = new Func<int, int>(sf1);
+																		Func<int, int> f2 = new Func<int, int>(sf2);
+
+																		//Func<int, int> f3 = new Func<int, int>(sf3);
+																		Func<int, int> f4 = new Func<int, int>(sf4);
+
+																		Func<int, int> f5 = new Func<int, int>(sf5);
+																		//Func<int, int> f6 = new Func<int, int>(sf6);
+
+																		//Func<int, int> f7 = new Func<int, int>(sf7);
+																		Func<int, int> f8 = new Func<int, int>(sf8);
+
+																		Func<int, int> f9 = new Func<int, int>(sf9);
+																		//Func<int, int> f10 = new Func<int, int>(sf10);
+
+																		Func<int, int> f11 = new Func<int, int>(sf11);
+																		Func<int, int> f12 = new Func<int, int>(sf12);
+
+																		//Func<int, int> f13 = new Func<int, int>(sf13);
+																		//Func<int, int> f14 = new Func<int, int>(sf14);
+
+																		//Func<int, int> f15 = new Func<int, int>(sf15);
+
+																		Func<int, int> function = new Func<int, int>(
+																		(value) =>
+																		(
+																			op7(
+																				(
+																					op3(
+																						(
+																							op1(
+																								f1(value), f2(value)
+																							)
+																						),
+																						(
+																							op2(
+																								f4(value), f5(value)
+																							)
+																						)
+																					)
+																				),
+																				(
+																					op6(
+																						(
+																							op4(
+																								f8(value), f9(value)
+																							)
+																						),
+																						(
+																							op5(
+																								f11(value), f12(value)
+																							)
+																						)
+																					)
+																				)
+																			)
+																		)
+																		);
+
+																		//Console.WriteLine(makeHumanReadable(create(function, 5)));
+
+																		if (createMakesMatchingSequence(function, questionSequence))
+																		{
+																			return function;
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			return null;
+		}
+
+		private static string makeHumanReadable(List<int> list)
+		{
+			List<int?> listWithNullables = list.ConvertAll<int?>(delegate(int i) { return i; });
+			return makeHumanReadable(listWithNullables);
+		}
+
+		private static string makeHumanReadable(List<int?> list)
+		{
+			String outStr = "";
+
+			outStr = "{";
+
+			foreach (int? item in list)
+			{
+				string itemStr = (item == null) ? "NULL" : item.ToString();
+				string extraSpace = (item != null && item >= 0) ? " " : "";
+				outStr = outStr + extraSpace + itemStr + ", ";
+			}
+
+			if (outStr.Length > 1)
+			{
+				outStr = outStr.Substring(0, outStr.Length - 2); // To cut of the last ", "
+			}
+
+			outStr = outStr + "}";
+
+			return outStr;
+		}
+
+		public  static List<int> map(Func<int, int> function, List<int> list)
+		{
+			List<int> newList = new List<int>();
+			for (int i = 0; i < list.Count; i++)
+			{
+				newList.Add(function(list[i])); //This is right
+			}
+			return newList;
+		}
+
+		public static List<int> map(Func<int, int, int> op, List<int> list1, List<int> list2)
+		{
+			if (list1.Count != list2.Count) throw new Exception("This shouldn't happen!");
+
+			List<int> newList = new List<int>();
+			for (int i = 0; i < list1.Count; i++)
+			{
+				newList.Add(op(list1[i], list2[i]));
+			}
+			return newList;
+		}
+
+	}
+}
---
+++ SmartieIQ1/TestSequenceHelper.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SmartieIQ
+{
+	[TestFixture]
+	public class TestSequenceHelper
+	{
+
+		private static List<int?> a1 = new List<int?> { 1, 2, 3, null };
+		private static List<int?> b1 = new List<int?> { 1, 2, 3, null, 5 };
+		private static List<int?> c1 = new List<int?> { 2, 4, 6, null, 10 };
+
+		private static List<int> a2 = new List<int> { 1, 2, 3, 4 };
+		private static List<int> b2 = new List<int> { 1, 2, 3, 4, 5 };
+		private static List<int> c2 = new List<int> { 2, 4, 6, 8, 10 };
+
+		[Test]
+		public static void isSame()
+		{
+
+			//int?
+			Assert.True(SequenceHelper.isSame(a1, a1));
+			Assert.True(SequenceHelper.isSame(b1, b1));
+			Assert.True(SequenceHelper.isSame(c1, c1));
+
+			Assert.False(SequenceHelper.isSame(a1, b1));
+			Assert.False(SequenceHelper.isSame(a1, c1));
+			Assert.False(SequenceHelper.isSame(b1, a1));
+			Assert.False(SequenceHelper.isSame(b1, c1));
+			Assert.False(SequenceHelper.isSame(c1, a1));
+			Assert.False(SequenceHelper.isSame(c1, b1));
+
+
+			//int
+			Assert.True(SequenceHelper.isSame(a2, a2));
+			Assert.True(SequenceHelper.isSame(b2, b2));
+			Assert.True(SequenceHelper.isSame(c2, c2));
+
+			Assert.False(SequenceHelper.isSame(a2, b2));
+			Assert.False(SequenceHelper.isSame(a2, c2));
+			Assert.False(SequenceHelper.isSame(b2, a2));
+			Assert.False(SequenceHelper.isSame(b2, c2));
+			Assert.False(SequenceHelper.isSame(c2, a2));
+			Assert.False(SequenceHelper.isSame(c2, b2));
+		}
+
+		[Test]
+		public static void fillMissing()
+		{
+			Assert.True(SequenceHelper.isSame(a2, SequenceHelper.fillMissing(a1)));
+			Assert.True(SequenceHelper.isSame(b2, SequenceHelper.fillMissing(b1)));
+			Assert.True(SequenceHelper.isSame(c2, SequenceHelper.fillMissing(c1)));
+
+			Assert.False(SequenceHelper.isSame(a2, SequenceHelper.fillMissing(b1)));
+			Assert.False(SequenceHelper.isSame(b2, SequenceHelper.fillMissing(c1)));
+			Assert.False(SequenceHelper.isSame(c2, SequenceHelper.fillMissing(a1)));
+		}
+
+		[Test]
+		public static void create()
+		{
+
+			Func<int, int> identityPlus1 = (value) => value + 1;
+			Func<int, int> identityPlus1Times2 = (value) => (value + 1) * 2;
+
+			Assert.True(SequenceHelper.isSame(a2, SequenceHelper.create(identityPlus1, 4)));
+			Assert.True(SequenceHelper.isSame(b2, SequenceHelper.create(identityPlus1, 5)));
+			Assert.True(SequenceHelper.isSame(c2, SequenceHelper.create(identityPlus1Times2, 5)));
+
+			Assert.False(SequenceHelper.isSame(c2, SequenceHelper.create(identityPlus1, 4)));
+			Assert.False(SequenceHelper.isSame(c2, SequenceHelper.create(identityPlus1, 5)));
+			Assert.False(SequenceHelper.isSame(a2, SequenceHelper.create(identityPlus1Times2, 5)));
+			Assert.False(SequenceHelper.isSame(b2, SequenceHelper.create(identityPlus1Times2, 5)));
+		}
+
+		[Test]
+		public static void getIdentitySequence()
+		{
+			Assert.True(SequenceHelper.isSame(new List<int> { 0 }, SequenceHelper.getIdentitySequence(1)));
+			Assert.True(SequenceHelper.isSame(new List<int> { 0, 1 }, SequenceHelper.getIdentitySequence(2)));
+			Assert.True(SequenceHelper.isSame(new List<int> { 0, 1, 2 }, SequenceHelper.getIdentitySequence(3)));
+			Assert.True(SequenceHelper.isSame(new List<int> { 0, 1, 2, 3 }, SequenceHelper.getIdentitySequence(4)));
+			Assert.True(SequenceHelper.isSame(new List<int> { 0, 1, 2, 3, 4 }, SequenceHelper.getIdentitySequence(5)));
+
+			Assert.False(SequenceHelper.isSame(new List<int> { 0, 1, 1000, 3, 4 }, SequenceHelper.getIdentitySequence(5)));
+		}
+
+		[Test]
+		public static void findItemGeneratorFunction()
+		{
+			Assert.True(SequenceHelper.isSame(a2, SequenceHelper.create(SequenceHelper.findItemGeneratorFunction(a1), a2.Count)));
+			Assert.True(SequenceHelper.isSame(b2, SequenceHelper.create(SequenceHelper.findItemGeneratorFunction(b1), b2.Count)));
+			Assert.True(SequenceHelper.isSame(c2, SequenceHelper.create(SequenceHelper.findItemGeneratorFunction(c1), c2.Count)));
+		}
+
+		[Test]
+		public static void createMakesMatchingSequence()
+		{
+			Func<int, int> identityPlus1 = (value) => value + 1;
+			Func<int, int> identityPlus1Times2 = (value) => (value + 1) * 2;
+
+			Assert.True(SequenceHelper.createMakesMatchingSequence(identityPlus1, a1));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(identityPlus1, b1));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(identityPlus1Times2, c1));
+
+			Assert.False(SequenceHelper.createMakesMatchingSequence(identityPlus1Times2, b1));
+			Assert.False(SequenceHelper.createMakesMatchingSequence(identityPlus1, c1));
+
+		}
+
+
+		/*
+		[Test]
+		public static void getFunctionTree()
+		{
+			//General function math
+			Func<int, int, int> add = new Func<int, int, int>((i, j) => i + j);
+			Func<int, int> one = new Func<int, int>((i) => 1);
+			Func<int, int> identity = new Func<int, int>((i) => i);
+
+			Func<int, int> twoiFunc = new Func<int, int>((value) => add(value, value));
+			Func<int, int> twoFunc = new Func<int, int>((value) => add(one(value), one(value)));
+			Func<int, int> addOneFunc = new Func<int, int>((value) => add(one(value), identity(value)));
+
+			Assert.True(SequenceHelper.createMakesMatchingSequence(twoiFunc, new List<int?> { 0, 2, 4, 6 }));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(twoFunc, new List<int?> { 2, 2, 2, 2 }));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(addOneFunc, new List<int?> { 1, 2, 3, 4 }));
+			//End - General function math
+			
+
+			
+			
+			List<Func<int, int>> functionTree = SequenceHelper.getFunctionTree(0);
+			Assert.AreEqual(1, functionTree.Count); //Identity only
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[0], new List<int?> { 0, 1, 2, 3 }));
+			
+			functionTree = SequenceHelper.getFunctionTree(1);
+			Assert.AreEqual(SequenceHelper.basic1ArgFuncs.Count, functionTree.Count);
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[0], new List<int?>{0,1,2,3}));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[1], new List<int?> { 0, 0, 0, 0 }));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[2], new List<int?> { 1,1,1,1}));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[3], new List<int?> { -1,-1,-1,-1 }));
+
+
+			functionTree = SequenceHelper.getFunctionTree(2);
+			//Assert.AreEqual(SequenceHelper.basic1ArgFuncs.Count, functionTree.Count);
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[0], new List<int?> { 0, 1, 2, 3 }));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[1], new List<int?> { 0, 0, 0, 0 }));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[2], new List<int?> { 1, 1, 1, 1 }));
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[3], new List<int?> { -1, -1, -1, -1 }));
+			
+
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[4], new List<int?> { 0, 2, 4, 6 }));//f1 f1 +
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[5], new List<int?> { 0, 1, 2, 3 }));//f2 f1 +
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[6], new List<int?> { 1, 2, 3, 4 }));//f3 f1 +
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[7], new List<int?> { -1, 0, 1, 2 }));//f4 f1 + 
+
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[8], new List<int?> { 0, 1, 2, 3 }));//f1 f2 +
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[9], new List<int?> { 0, 0, 0, 0 }));//f2 f2 +
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[10], new List<int?> { 1, 1, 1, 1 }));//f3 f2 +
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[11], new List<int?> { -1, -1, -1, -1 }));//f4 f2 + 
+
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[12], new List<int?> { 1, 2, 3, 4 }));//f1 f3 +
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[13], new List<int?> { 1,1, 1, 1 }));//f2 f3 +
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[14], new List<int?> { 2, 2, 2, 2 }));//f3 f3 +
+			Assert.True(SequenceHelper.createMakesMatchingSequence(functionTree[15], new List<int?> { 0, 0, 0, 0 }));//f4 f3 + 
+
+	}
+*/
+
+
+		[Test]
+		public static void map()
+		{
+
+			Func<int, int> identityPlus1 = (value) => value + 1;
+			Func<int, int> identityPlus1Times2 = (value) => (value + 1) * 2;
+
+			Assert.True(SequenceHelper.isSame(a2, SequenceHelper.map(identityPlus1, SequenceHelper.getIdentitySequence(a2.Count))));
+			Assert.True(SequenceHelper.isSame(b2, SequenceHelper.map(identityPlus1, SequenceHelper.getIdentitySequence(b2.Count))));
+			Assert.True(SequenceHelper.isSame(c2, SequenceHelper.map(identityPlus1Times2, SequenceHelper.getIdentitySequence(c2.Count))));
+
+			Assert.False(SequenceHelper.isSame(a2, SequenceHelper.map(identityPlus1Times2, SequenceHelper.getIdentitySequence(a2.Count))));
+			Assert.False(SequenceHelper.isSame(b2, SequenceHelper.map(identityPlus1Times2, SequenceHelper.getIdentitySequence(b2.Count))));
+			Assert.False(SequenceHelper.isSame(c2, SequenceHelper.map(identityPlus1, SequenceHelper.getIdentitySequence(c2.Count))));
+
+
+			Func<int, int, int> add = (value1, value2) => value1 + value2;
+			Func<int, int, int> mult = (value1, value2) => value1 * value2;
+
+			List<int> a3 = new List<int> { 2, 2, 2, 2 };
+			List<int> b3 = new List<int> { 1, 2, 3, 4 };
+
+			List<int> aAddB = new List<int> { 3, 4, 5, 6 };
+			List<int> aMultB = new List<int> { 2, 4, 6, 8 };
+
+			Assert.True(SequenceHelper.isSame(aAddB, SequenceHelper.map(add, a3, b3)));
+			Assert.True(SequenceHelper.isSame(aMultB, SequenceHelper.map(mult, a3, b3)));
+
+			Assert.False(SequenceHelper.isSame(aMultB, SequenceHelper.map(add, a3, b3)));
+			Assert.False(SequenceHelper.isSame(aAddB, SequenceHelper.map(mult, a3, b3)));
+
+		}
+	}
+}
